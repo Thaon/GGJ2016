d6782cf2483684f3310777baac6d5f8eafb31f20
Thaon <gabriele.maddaloni@gmail.com>
2016-01-30 16:45:01 +0000

    Added artsy fuckery

---
+++ Assets/KeyPiece.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPiece : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject.FindWithTag("MainCamera").GetComponent<FancyPantsScript>().UpdateKey();
+            Destroy(this.gameObject);
+        }
+    }
+}
