861920d4a320d8418cb4404699fe6d5c3c527223
Thaon <gabriele.maddaloni@gmail.com>
2016-01-31 01:47:00 +0000

    dummy file created for no reason

---
+++ Assets/Code/Dummy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dummy : MonoBehaviour {
+
+    int var = 5;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("I was created");
+        Debug.Log(var);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
